66b775d9c5f58a025cc1163aa1d23e36eeeb38d7
Thiago Rocha <kimobr@gmail.com>
2012-11-06 22:45:47 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BibleService::Application.config.secret_token = '7923b1315c4e2a9a968761603c8bda5b76f11b9b9e7d358dea364805bf77ca150e80721b147af954748ffbf2cc0feb11de29aa044e073e6e957a27237f0b6bc4'

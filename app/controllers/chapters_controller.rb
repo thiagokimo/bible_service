ca099aba12412698586fdb31cbddbb75703c3422
Kimo <kimobr@gmail.com>
2012-11-07 08:32:32 -0200

    refatoracao do controller de capitulos

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,8 +1,10 @@
 class ChaptersController < ApplicationController
+   before_filter :load_book
+
   # GET /chapters
   # GET /chapters.json
   def index
-    @chapters = Chapter.all
+    @chapters = @book.chapters
 
     respond_to do |format|
       format.html # index.html.erb
@@ -21,63 +23,9 @@ class ChaptersController < ApplicationController
     end
   end
 
-  # GET /chapters/new
-  # GET /chapters/new.json
-  def new
-    @chapter = Chapter.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @chapter }
-    end
-  end
-
-  # GET /chapters/1/edit
-  def edit
-    @chapter = Chapter.find(params[:id])
-  end
-
-  # POST /chapters
-  # POST /chapters.json
-  def create
-    @chapter = Chapter.new(params[:chapter])
-
-    respond_to do |format|
-      if @chapter.save
-        format.html { redirect_to @chapter, notice: 'Chapter was successfully created.' }
-        format.json { render json: @chapter, status: :created, location: @chapter }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @chapter.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /chapters/1
-  # PUT /chapters/1.json
-  def update
-    @chapter = Chapter.find(params[:id])
-
-    respond_to do |format|
-      if @chapter.update_attributes(params[:chapter])
-        format.html { redirect_to @chapter, notice: 'Chapter was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @chapter.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /chapters/1
-  # DELETE /chapters/1.json
-  def destroy
-    @chapter = Chapter.find(params[:id])
-    @chapter.destroy
-
-    respond_to do |format|
-      format.html { redirect_to chapters_url }
-      format.json { head :no_content }
-    end
+  protected
+  def load_manga
+    @book = Book.find(params[:book_id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121107004733) do
+ActiveRecord::Schema.define(:version => 20121107093219) do
 
   create_table "books", :force => true do |t|
     t.string   "name"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(:version => 20121107004733) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "chapters", :force => true do |t|
+    t.integer  "number"
+    t.string   "link"
+    t.integer  "book_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "chapters", ["book_id"], :name => "index_chapters_on_book_id"
 
 end

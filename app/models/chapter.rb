99b752e68d8c2d0bd7cb4344f5a8c573cd0b3a2d
Kimo <kimobr@gmail.com>
2012-11-07 07:38:09 -0200

    adiciona validacoes no modelo de capitulo

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,4 +1,7 @@
 class Chapter < ActiveRecord::Base
   belongs_to :book
   attr_accessible :link, :number
+  validates :link, :uniqueness => { :case_sensitive => false }
+  
+  validates_presence_of :number, :link
 end
